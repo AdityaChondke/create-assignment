698024dba0be07aed4a2e46300e273deb5781e88
Aditya Chondke <aditya.chondke@gmail.com>
2023-06-22 04:23:43 +0530

    complete assignment

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,15 +3,12 @@ import './Card.css';
 import Transactions from './Transactions';
 import UserDetails from './UserDetails';
 
-const Card = ({ index , handleClick, cardNumber}) => {
+const Card = ({ index , handleClick, cardNumber, showTransactionsMethod}) => {
   return (
-    <div
-      id={`card-${index}`}
-      className={`card card-${index}`}
-      onClick={() => handleClick(index)}
-    >
+    <div id={`card-${index}`} className={`card card-${index}`} onClick={() => handleClick(index)}>
     <div className="card-icon"><img src={`./images/chip-icon-${index}.svg`}/></div>
       <div className='card-number'>{cardNumber}</div>
+      {index === 2 && <div className='view-details-button' onClick={showTransactionsMethod}>View card details</div> }
       <div className="card-type"><img src={`./images/chip-type-${index}.svg`}/></div>
     </div>
   );
@@ -21,68 +18,97 @@ const CardStack = () => {
     const [cardSwiped, setCardSwiped] = useState([false,false,false,false]);
     const [showTransactions, setShowTransactions] = useState(false);
     const [showUserDetails, setShowUserDetails] = useState(false);
+    const [activeIndex, setActiveIndex] = useState([0]);
+
+
+    const showTransactionsMethod = (event) => {
+        if(activeIndex[1] != 2 || showUserDetails){
+            return;
+        }
+        event.stopPropagation();
+        document.getElementById(`card-2`).style.transform  = 'scale(0.95)';
+
+        setTimeout(function() {
+            document.getElementById(`card-2`).style.transform  = 'scale(1)';
+        }, 100);
+        
+        setTimeout(function() {
+            setShowTransactions(true);
+            document.getElementById(`transactions`).style.transform = 'translateY(-1200px)';
+            document.getElementById(`transactions`).style.zIndex = 10;
+            document.getElementById(`button`).style.transform = 'translateY(-200px)';
+            document.getElementById(`button`).style.zIndex = 0;
+            document.getElementById(`big-title`).style.transform = 'translateX(-200px)';
+            document.getElementById(`big-title`).style.zIndex = -1;
+    
+            for(let i = 2 + 1; i < 4; i++){
+                document.getElementById(`card-${i}`).style.transform = 'translateY(0px) rotateX(0deg) scale(0) translateZ(-100px)';
+                document.getElementById(`card-${i}`).style.zIndex = -4 ;
+            }
+            for(let i = 0 ; i < 2; i++){
+                document.getElementById(`card-${i}`).style.transform = 'translateY(0px) rotateX(0deg) scale(0) translateZ(-400px)';
+                document.getElementById(`card-${i}`).style.zIndex = -4 ;   
+            }
+          }, 600); 
+                 
+
+    }
+
+
+    const showUserDetailsMethod = () => {
+        setShowUserDetails(true);
+        document.getElementById(`see-details`).style.transform = 'translateY(0px)';
+        document.getElementById(`transactions`).style.transform = 'translateY(-1000px)';
+        document.getElementById(`transactions-list`).style.display = 'none';
+        document.getElementById(`card-2`).style.transform = 'translateY(200px)';
+        document.getElementById(`user-details`).style.transform = 'translateY(420px)';
+        document.getElementById(`triangle-2`).style.transform ='translate(140px, -100px) rotateX(180deg)';
+        document.getElementById(`triangle-1`).style.transform ='translate(0px, 0px) rotateX(180deg)';
+
+    }
 
     const handleClick = (index) => {
-
-
         if(index === 3){
             return;
         }
+        if(!activeIndex.includes(index)){
+            return;
+        }
 
-        // if(showTransactions && index === 2){
-        //     setShowUserDetails(true);
-        //     document.getElementById(`transactions`).style.transform = 'translateY(-900px)';
-        //     document.getElementById(`transactions-list`).style.display = 'none';
-        //     document.getElementById(`card-2`).style.transform = 'translateY(250px)';
-        //     document.getElementById(`user-details`).style.transform = 'translateY(420px)';
-        // }
+        if(showTransactions && index === 2){
+            showUserDetailsMethod();
+            return;
+        }
 
-        // if(!showTransactions && index === 2){
-        //     setShowTransactions(true);
-        //     document.getElementById(`transactions`).style.transform = 'translateY(-1200px)';
-        //     document.getElementById(`transactions`).style.zIndex = 10;
-        //     document.getElementById(`button`).style.transform = 'translateY(-200px)';
-        //     document.getElementById(`button`).style.zIndex = 0;
-        //     document.getElementById(`big-title`).style.transform = 'translateX(-200px)';
-        //     document.getElementById(`big-title`).style.zIndex = -1;
-
-
-        //     for(let i = index + 1; i < 4; i++){
-        //         document.getElementById(`card-${i}`).style.transform = 'translateY(0px) rotateX(0deg) scale(0) translateZ(-100px)';
-        //         document.getElementById(`card-${i}`).style.zIndex = -4 ;
-        //     }
-        //     for(let i = 0 ; i < index; i++){
-        //         document.getElementById(`card-${i}`).style.transform = 'translateY(0px) rotateX(0deg) scale(0) translateZ(-400px)';
-        //         document.getElementById(`card-${i}`).style.zIndex = -4 ;   
-        //     } 
-            
-        // }
 
         if(cardSwiped[index]) {
-
             if(index ===0) {
+                setActiveIndex([0]);
+                document.getElementById(`no-cards`).style.transform = 'translateY(-300px)';
                 document.getElementById(`rectangle-2`).style.transform = 'rotate(0deg) translateX(-50px)';
                 document.getElementById(`rectangle-1`).style.transform = 'rotate(0deg)';
                 document.getElementById(`polygon-2`).style.transform= 'translateX(100px) rotate(90deg)';
                 document.getElementById(`polygon-1`).style.transform= 'translateX(-50px) rotate(-57deg)';   
             }
             if(index === 1) {
-
-            document.getElementById(`polygon-2`).style.transform= 'translateX(230px) rotate(0deg)';
-            document.getElementById(`polygon-1`).style.transform= 'translateX(10px)';
-            document.getElementById(`triangle-2`).style.transform = 'translate(300px,220px)';
-            document.getElementById(`triangle-1`).style.transform ='translate(-200px,-10px)';
+                setActiveIndex([0,1]);
+                document.getElementById(`polygon-2`).style.transform= 'translateX(230px) rotate(0deg)';
+                document.getElementById(`polygon-1`).style.transform= 'translateX(10px)';
+                document.getElementById(`triangle-2`).style.transform = 'translate(300px,220px)';
+                document.getElementById(`triangle-1`).style.transform ='translate(-200px,-10px)';
             }
 
             if(index === 2) {
+                setActiveIndex([1,2]);
                 document.getElementById(`circle-2`).style.transform = 'translateX(400px)';
-                document.getElementById(`circle-1`).style.transform = 'translateX(-400px)';
+                document.getElementById(`circle-1`).style.transform = 'translateX(-200px)';
                 document.getElementById(`triangle-2`).style.transform ='translate(140px, 20px)';
                 document.getElementById(`triangle-1`).style.transform ='translate(0px, 20px)';
             }
 
             document.getElementById(`card-${index}`).style.transform = 'translateY(0px) rotateX(0deg)';
             document.getElementById(`card-${index}`).style.zIndex = -index ;
+
             setCardSwiped((prev) => {
                 return prev.map((item, i) => {
                     if(i === index){
@@ -101,7 +127,7 @@ const CardStack = () => {
                 document.getElementById(`card-${i}`).style.transform = `translateY(300px) rotateX(45deg) translateZ(${updatedTrasformZValue}px)`;  
             }
         }else{
-            // if(index !== 2) {
+    
             setCardSwiped((prev) => {
             return prev.map((item, i) => {
                 if(i === index){
@@ -113,18 +139,22 @@ const CardStack = () => {
         });
     
         if(index === 0) {
+            setActiveIndex([0,1]);
+            document.getElementById(`no-cards`).style.transform = 'translateY(0px)';
             document.getElementById(`rectangle-2`).style.transform = 'rotate(46deg) translateX(100px)';
             document.getElementById(`rectangle-1`).style.transform = 'rotate(-33deg)';
             document.getElementById(`polygon-2`).style.transform= 'translateX(230px) rotate(0deg)';
             document.getElementById(`polygon-1`).style.transform= 'translateX(10px)';
         }
         if(index === 1) {
+            setActiveIndex([1,2]);
             document.getElementById(`polygon-2`).style.transform= 'translateX(100px) rotate(90deg)';
-                document.getElementById(`polygon-1`).style.transform= 'translateX(-50px) rotate(-57deg)';
+            document.getElementById(`polygon-1`).style.transform= 'translateX(-50px) rotate(-57deg)';
             document.getElementById(`triangle-2`).style.transform ='translate(140px, 20px)';
             document.getElementById(`triangle-1`).style.transform ='translate(0px, 20px)';
         }
         if(index === 2) {
+            setActiveIndex([2,3]);
             document.getElementById(`triangle-2`).style.transform = 'translate(300px,220px)';
             document.getElementById(`triangle-1`).style.transform ='translate(-200px,-10px)';
             document.getElementById(`circle-2`).style.transform = 'translateX(240px)';
@@ -142,7 +172,7 @@ const CardStack = () => {
             document.getElementById(`card-${i}`).style.transform = `translateY(300px) rotateX(45deg) translateZ(${updatedTrasformZValue}px)`;  
             document.getElementById(`card-${index}`).style.zIndex = index ;
         }
-    // }
+
 } 
 };
 
@@ -157,35 +187,44 @@ const showCards = () => {
     document.getElementById(`card-1`).style.transform = `translateY(300px) rotateX(45deg) translateZ(0px)`;  
     document.getElementById(`card-0`).style.zIndex = 0 ;
     document.getElementById(`card-1`).style.zIndex = 1 ;
-    
+    document.getElementById(`triangle-2`).style.transform ='translate(140px, 20px)';
+    document.getElementById(`triangle-1`).style.transform ='translate(0px, 20px)'; 
+    document.getElementById(`button`).style.transform = 'translateX(0px)';
+    document.getElementById(`button`).style.zIndex = -1;
+    document.getElementById(`big-title`).style.transform = 'translateX(0px)';
+    document.getElementById(`big-title`).style.zIndex = 0;
 };
 
-const cardNumbers=['8123 6872 4193 1337','1952 7458 9151 5734','8714 1782 4237 1786','5183 1851 5913 0145']
+    const cardNumbers=['8123 6872 4193 1337','1952 7458 9151 5734','8714 1782 4237 1786','5183 1851 5913 0145']
 
-  const cards = Array.from({ length: 4 }, (_, index) => <Card key={index} index={index} cardNumber={cardNumbers[index]}  handleClick={handleClick} />);
+  const cards = Array.from({ length: 4 }, (_, index) => <Card key={index} index={index} cardNumber={cardNumbers[index]}  handleClick={handleClick} showTransactionsMethod={showTransactionsMethod} />);
   return <>
- <div className='shapes'>
-  <div className='shapes rectangle' id='rectangle-1' ><img src={`./images/rectangle-1.svg`}  style={{transform: 'skew(10deg, 10deg)'}}/></div>
-  <div className='shapes rectangle-2' id='rectangle-2' ><img src={`./images/rectangle-2.svg`} style={{transform: 'skew(10deg, 10deg)'}}/></div>
-  <div className='shapes polygon-2' id='polygon-2' ><img src={`./images/polygon-2.svg`} style={{width: '200px', height:'200px'}}/></div>
-  <div className='shapes polygon-1' id='polygon-1' ><img src={`./images/polygon-1.svg`} style={{width: '260px', height:'260px'}}/></div>
-  <div className='shapes triangle-2' id='triangle-2' ><img src={`./images/triangle-2.svg`} /></div>
-  <div className='shapes triangle-1' id='triangle-1' ><img src={`./images/triangle-1.svg`} /></div>
-  <div className='shapes circle-2' id='circle-2' ><img src={`./images/circle-2.svg`} style={{width: '180px', height:'180px'}} /></div>
-  <div className='shapes circle-1' id='circle-1' ><img src={`./images/circle-1.svg`} style={{width: '140px', height:'140px'}} /></div>
-  </div>
-  <div className="button-container">
-     <div className="button" id='button' onClick={showCards}><img src={`./images/arrow-left.svg`}/></div> 
-    <div className='big-title' id='big-title'> Your Cards</div>
-    <div className="button-text">VR</div>
-  </div>
-  <div style={{height:'700px'}}>
+        <div className='shapes'>
+            <div className='shapes rectangle' id='rectangle-1' ><img src={`./images/rectangle-1.svg`}  style={{transform: 'skew(10deg, 10deg)'}}/></div>
+            <div className='shapes rectangle-2' id='rectangle-2' ><img src={`./images/rectangle-2.svg`} style={{transform: 'skew(10deg, 10deg)'}}/></div>
+            <div className='shapes polygon-2' id='polygon-2' ><img src={`./images/polygon-2.svg`} style={{width: '200px', height:'200px'}}/></div>
+            <div className='shapes polygon-1' id='polygon-1' ><img src={`./images/polygon-1.svg`} style={{width: '260px', height:'260px'}}/></div>
+            <div className='shapes triangle-2' id='triangle-2' ><img src={`./images/triangle-2.svg`} /></div>
+            <div className='shapes triangle-1' id='triangle-1' ><img src={`./images/triangle-1.svg`} /></div>
+            <div className='shapes circle-2' id='circle-2' ><img src={`./images/circle-2.svg`} style={{width: '180px', height:'180px'}} /></div>
+            <div className='shapes circle-1' id='circle-1' ><img src={`./images/circle-1.svg`} style={{width: '140px', height:'140px'}} /></div>
+        </div>
+
+        <div className="button-container">
+            <div className="button" id='button' onClick={showCards}><img src={`./images/arrow-left.svg`}/></div> 
+            <div className='big-title' id='big-title'> Your Cards</div>
+            <div className="button-text">VR</div>
+        </div>
   
-  <div className='user-details-root' id='user-details' >{showUserDetails &&  <UserDetails />}</div>
-  <div className='card-stack'>{cards.reverse()}</div>
-  <div className='transactions-root' id="transactions">{showTransactions &&  <Transactions />}</div>
-  </div>
-   
+        <div style={{height:'700px'}}>
+            <div className='user-details-root' id='user-details' >{showUserDetails &&  <UserDetails />}</div>
+            <div className='card-stack'>{cards.reverse()}
+           
+            </div>
+            <div className='no-cards' id='no-cards' >No more cards</div>
+            {showTransactions && <div className='see-details' id='see-details' >Click on card to see details</div>}
+            <div className='transactions-root' id="transactions">{showTransactions &&  <Transactions />}</div>
+         </div>
   </>;
 };
 
--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -9,50 +9,58 @@ export default function Transactions() {
         <div className='transaction-entry'>
         <div className='transaction-image' > <img src={`./images/ticket.svg`} alt="Ticket" /></div>
         <div className='transaction-details'>
-            <div className='transaction-title'>Entry 1 </div>
-            <div className='transaction-discription'>Details about entry</div>
+            <div className='transaction-title'>Movie Ticket</div>
+            <div className='transaction-discription'>6 hours ago</div>
         </div>
-        <div className='transaction-amount'>Amount</div>
-        </div>
-        <div className='transaction-entry'>
-        <div className='transaction-image' > <img src={`./images/ticket.svg`} alt="Ticket" /></div>
-        <div className='transaction-details'>
-            <div className='transaction-title'>Entry 1 </div>
-            <div className='transaction-discription'>Details about entry</div>
-        </div>
-        <div className='transaction-amount'>Amount</div>
+        <div className='transaction-amount'><img src="http://i.stack.imgur.com/nGbfO.png" width="10" height="12"/>  725</div>
         </div>
         <div className='transaction-entry'>
-        <div className='transaction-image' > <img src={`./images/ticket.svg`} alt="Ticket" /></div>
+        <div className='transaction-image' > <img src={`./images/subscription.svg`} alt="Ticket" /></div>
         <div className='transaction-details'>
-            <div className='transaction-title'>Entry 1 </div>
-            <div className='transaction-discription'>Details about entry</div>
+            <div className='transaction-title'>Subscription - Medium</div>
+            <div className='transaction-discription'>Yesterday</div>
         </div>
-        <div className='transaction-amount'>Amount</div>
+        <div className='transaction-amount'><img src="http://i.stack.imgur.com/nGbfO.png" width="10" height="12"/>  325</div>
         </div>
         <div className='transaction-entry'>
-        <div className='transaction-image' > <img src={`./images/ticket.svg`} alt="Ticket" /></div>
+        <div className='transaction-image' > <img src={`./images/credit-card.svg`} alt="Ticket" /></div>
         <div className='transaction-details'>
-            <div className='transaction-title'>Entry 1 </div>
-            <div className='transaction-discription'>Details about entry</div>
+            <div className='transaction-title'>Credit card payment</div>
+            <div className='transaction-discription'>2 days ago</div>
         </div>
-        <div className='transaction-amount'>Amount</div>
+        <div className='transaction-amount'><img src="http://i.stack.imgur.com/nGbfO.png" width="10" height="12"/> 14,983</div>
         </div>
         <div className='transaction-entry'>
-        <div className='transaction-image' > <img src={`./images/ticket.svg`} alt="Ticket" /></div>
+        <div className='transaction-image' > <img src={`./images/delivery.svg`} alt="Ticket" /></div>
         <div className='transaction-details'>
-            <div className='transaction-title'>Entry 1 </div>
-            <div className='transaction-discription'>Details about entry</div>
+            <div className='transaction-title'>Food delivery</div>
+            <div className='transaction-discription'>Last week</div>
         </div>
-        <div className='transaction-amount'>Amount</div>
+        <div className='transaction-amount'><img src="http://i.stack.imgur.com/nGbfO.png" width="10" height="12"/> 459</div>
         </div>
         <div className='transaction-entry'>
-        <div className='transaction-image' > <img src={`./images/ticket.svg`} alt="Ticket" /></div>
+        <div className='transaction-image' > <img src={`./images/cart.svg`} alt="Ticket" /></div>
         <div className='transaction-details'>
-            <div className='transaction-title'>Entry 1 </div>
-            <div className='transaction-discription'>Details about entry</div>
+            <div className='transaction-title'>Amazon purchase</div>
+            <div className='transaction-discription'>Last week</div>
         </div>
-        <div className='transaction-amount'>Amount</div>
+        <div className='transaction-amount'><img src="http://i.stack.imgur.com/nGbfO.png" width="10" height="12"/> 12,209</div>
+        </div>
+        <div className='transaction-entry'>
+            <div className='transaction-image'><img src={`./images/delivery.svg`} alt="Ticket" /></div>
+         <div className='transaction-details'>
+            <div className='transaction-title'>Food delivery </div>
+            <div className='transaction-discription'>4th January 2021</div>
+         </div>
+        <div className='transaction-amount'><img src="http://i.stack.imgur.com/nGbfO.png" width="10" height="12"/> 1,245</div>
+        </div>
+        <div className='transaction-entry'>
+            <div className='transaction-image'><img src={`./images/subscription.svg`} alt="Ticket" /></div>
+         <div className='transaction-details'>
+            <div className='transaction-title'>Subscription - Medium</div>
+            <div className='transaction-discription'>28th December 2020</div>
+         </div>
+        <div className='transaction-amount'><img src="http://i.stack.imgur.com/nGbfO.png" width="10" height="12"/> 325</div>
         </div>
     </div>
     </div>
